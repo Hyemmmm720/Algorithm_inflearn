92caf5f76cddd426e9e7d12c3d8aa8192fce9c9e
Hyemi Lee <zzxxc0720@gmail.com>
2023-07-20 10:13:05 +0900

    6번 문제풀이

---
+++ section1/6.js
@@ -0,0 +1,19 @@
+// 홀수 판별하기
+
+function solution(arr) {
+  let answer = [];
+  let sum = 0,
+    min = Number.MAX_SAFE_INTEGER;
+  for (let item of arr) {
+    if (item % 2 === 1) sum += item;
+    if (item < min) min = item;
+
+    console.log(min); // 77, 38, 38, 38, 12, 12, 12
+  }
+  answer.push(sum);
+  answer.push(min);
+  return answer;
+}
+
+arr = [77, 38, 41, 12, 53, 92, 85];
+console.log(solution(arr));
