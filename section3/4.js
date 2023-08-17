816be4026ee7056ae60b42cdb1acfc450e7940c2
Hyemi Lee <zzxxc0720@gmail.com>
2023-08-17 09:36:21 +0900

    문제풀이

---
+++ section3/4.js
@@ -0,0 +1,47 @@
+// 가장 짧은 문자거리
+function solution(s, t) {
+  const answer = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      answer.push(0);
+    } else {
+      // 각 문자를 기준으로 왼쪽, 오른쪽을 나눠서 검사
+      const left = i === 0 ? 1000 : i - s.slice(0, i).lastIndexOf(t);
+      const right = i === s.length - 1 ? 1000 : s.slice(i + 1).indexOf(t) + 1;
+
+      answer.push(Math.min(left, right));
+    }
+  }
+
+  return answer.join(" ");
+}
+
+let str = "teachermode";
+console.log(solution(str, "e"));
+
+// 선생님 풀이
+function solution(s, t) {
+  let answer = [];
+  let p = 1000;
+
+  // 문자 t가 왼쪽에 있는 경우
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) p = 0;
+    else p++;
+
+    answer.push(p);
+  }
+
+  p = 1000;
+
+  // 문자 t가 오른쪽에 있는 경우
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) p = 0;
+    else p++;
+
+    answer[i] = Math.min(answer[i], p);
+  }
+
+  return answer;
+}
