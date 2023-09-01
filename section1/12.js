dd93c711ce71400bbe3c246afa677a3dca8675d3
Hyemi Lee <zzxxc0720@gmail.com>
2023-09-01 14:27:53 +0900

    문제풀이

--- section1/12.js
+++ section1/12.js
@@ -1,7 +1,11 @@
 // 대문자로 통일하기
+// 대문자, 소문자가 있는 문자열을 받아 대문자로 모두 통일해서 문자열 출력하기
+
+// 내 풀이
 function solution(s) {
   let answer = "";
   for (let x of s) {
+    // 문자열에 소문자를 찾아내서 대문자로 변경
     if (x === x.toLowerCase()) answer += x.toUpperCase();
     else answer += x;
   }
--- section1/13.js
+++ section1/13.js
@@ -1,9 +1,17 @@
 // 대문자로 통일
-const solution = (s) => {
-  console.log(s.toUpperCase());
-};
+// 대문자, 소문자가 존재하는 문자열을 받아 대문자는 소문자로, 소문자는 대문자로 바꿔 문자열을 출력하기
+function solution(s) {
+  let answer = "";
 
-solution("ItisTimeToStudy");
+  for (let x of s) {
+    if (x === x.toUpperCase()) answer += x.toLowerCase();
+    else answer += x.toUpperCase();
+  }
+
+  return answer;
+}
+
+console.log(solution("ItisTimeToStudy"));
 
 // 선생님 답안
 function solution(s) {
